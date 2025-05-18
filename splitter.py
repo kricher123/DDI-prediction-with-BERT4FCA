f21b18ab4fc7f8759f9355ea1b4f6979397402f2
csd3956 <128300493+kricher123@users.noreply.github.com>
2025-05-18 15:03:40 +0300

    Update splitter.py

--- splitter.py
+++ splitter.py
@@ -4,18 +4,13 @@ from slugify import slugify
 
 
 
-drugs=json.loads(Path("drugs.json").read_text())
-
-# for drug in drugs['drugbank']['drug']:
-#     filename = "drugs_split\\" + drug['name'] + ".json"
-#     print(filename)
-#     filename = slugify(filename)
-#     file = open(filename, "w+")
-#     json.dump(drug , file)
 
 
-file = open("drugs_split\\Acetaminophen.json" , "r")
-data = json.load(file)
-#print(data['categories']['category']['category'])
-for interaction in data["drug-interactions"]["drug-interaction"]:
-    print(interaction["name"])
+for drug in drugs['drugbank']['drug']:
+    filename = "drugs_split\\" + drug['name'] + ".json"
+    print(filename)
+    filename = slugify(filename)
+    file = open(filename, "w+")
+    json.dump(drug , file)
+
+
